ddd40f94a77079389e84e9a964dfb6f4ea859a3b
OstapchukAB <electron.anton@gmail.com>
2021-05-09 23:17:46 +0300

    Написал запрос через LINQ выводящий наименование поставщика, товара и его цену, отсортировал цены ниже 100$.

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace CSinDepth
         {
 
             List<Product> Lp = Product.GetSampleProducts();
+            List<Supplier> Lsp = Supplier.GetSampleSupplier();  
             //Lp.Sort(delegate (Product x, Product y)
             //{
             //    //return y.Price.CompareTo(x.Price);
@@ -28,14 +29,25 @@ namespace CSinDepth
             Lp.Add(new Product("Noname"));
 
             //C#3.0
-            foreach (Product p in Lp.OrderBy(x => x.Price).ThenBy(x => x.Name)
-                .Where(x => x.Price < 100 || x.Price == null))
-            {
-                //var price = p.Price.ToString();
-                Console.WriteLine(p.ToString());
-            }
+            //foreach (Product p in Lp.OrderBy(x => x.Price).ThenBy(x => x.Name)
+            //    .Where(x => x.Price < 100 || x.Price == null))
+            //{
+            //    //var price = p.Price.ToString();
+            //    Console.WriteLine(p.ToString());
+            //}
+            var filtered = from p in Lp
+                           join s in Lsp
+              on p.SupplierID equals s.SupplierID
+                           where p.Price < 100
+                           orderby p.Price,p.Name,s.Name    
+                           select new { SupName = s.Name, ProdName = p.Name, p.Price };
+                           
+
 
             //Console.WriteLine();
+            foreach (var v in filtered)
+                Console.WriteLine("{0}   {1}     {2:N2} $",
+                    v.SupName,v.ProdName,v.Price);  
 
             Console.ReadLine();
             
@@ -57,14 +69,14 @@ namespace CSinDepth
         {
             return new List<Product>
             {
-                new Product{Name="West side",Price=999.99m,SupplierID=1001},
+                new Product{Name="Wooding  ",Price=999.99m,SupplierID=1001},
                 new Product{Name="Assaian 2",Price=10.45m,SupplierID=1001 },
                 new Product{Name="Assaian  ",Price=1.45m,SupplierID=1001 },
                 new Product{Name="Zooming  ",Price=0.4m,SupplierID=null },
                 new Product{Name="Food     ",Price=9.99m,SupplierID=null },
                 new Product{Name="Qest     ",Price=9.99m,SupplierID=null},
-                new Product{Name="Sian     ",Price=1.75m ,SupplierID=2001},
-                new Product{Name="Foom     ",Price=1000.01m, SupplierID=2001 },
+                new Product{Name="Soop     ",Price=1.75m ,SupplierID=2001},
+                new Product{Name="Foot     ",Price=1000.01m, SupplierID=2001 },
                 new Product{Name="Botting  ",Price=56.41m,SupplierID=1001 },
                 new Product{Name="Botting 2",Price=null,SupplierID=null }
 
@@ -75,7 +87,7 @@ namespace CSinDepth
         {
             
             return string.Format("{2:D4}    {0}     {1:N2}",
-                Name,Price, SupplierID==null ? 0000:SupplierID);
+                Name,Price , SupplierID==null ? 0000:SupplierID);
         }
 
 
@@ -95,7 +107,7 @@ namespace CSinDepth
         {
             return new List<Supplier>
             {
-                new Supplier{Name="Apple",SupplierID=1001},
+                new Supplier{Name="Apple    ",SupplierID=1001},
                 new Supplier{Name="Microsoft",SupplierID=2001 }
 
             };
