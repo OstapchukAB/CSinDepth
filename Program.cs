717f80ecab1063ae465aa958104e1c5ac6a9efe5
OstapchukAB <electron.anton@gmail.com>
2021-05-11 17:54:21 +0300

    не могу реализовать внешнее соединение, выскакивает ошибка System.NullReferenceException

--- Program.cs
+++ Program.cs
@@ -35,19 +35,26 @@ namespace CSinDepth
             //    //var price = p.Price.ToString();
             //    Console.WriteLine(p.ToString());
             //}
-            var filtered = from p in Lp
-                           join s in Lsp
-              on p.SupplierID equals s.SupplierID
-                           where p.Price < 100
-                           orderby p.Price,p.Name,s.Name    
-                           select new { SupName = s.Name, ProdName = p.Name, p.Price };
-                           
+            var v_product = from p in Lp
+                                 select new {Pname = p.Name, p.Price,p.SupplierID};
+
+            //Left outer JOIN System.NullReferenceException
+
+            var result = from p in v_product
+                         join s in Lsp on p.SupplierID equals s.SupplierID into PS
+                         from res in PS.DefaultIfEmpty()
+                         select new {
+                             Sname= (res.Name ==null) ? default(string):res.Name,
+                             p.Pname,
+                             Price = (p.Price ==null) ? default(decimal) : p.Price
+                         };
+
 
 
             //Console.WriteLine();
-            foreach (var v in filtered)
-                Console.WriteLine("{0}   {1}     {2:N2} $",
-                    v.SupName,v.ProdName,v.Price);  
+            foreach (var v in result)
+                Console.WriteLine("{0}      {1}     {2}",
+            v.Sname ,v.Pname,v.Price);  
 
             Console.ReadLine();
             
@@ -69,55 +76,61 @@ namespace CSinDepth
         {
             return new List<Product>
             {
-                new Product{Name="Wooding  ",Price=999.99m,SupplierID=1001},
+                new Product{Name="Smartph ",Price=999.99m,SupplierID=1001},
                 new Product{Name="Assaian 2",Price=10.45m,SupplierID=1001 },
                 new Product{Name="Assaian  ",Price=1.45m,SupplierID=1001 },
-                new Product{Name="Zooming  ",Price=0.4m,SupplierID=null },
-                new Product{Name="Food     ",Price=9.99m,SupplierID=null },
-                new Product{Name="Qest     ",Price=9.99m,SupplierID=null},
+                new Product{Name="Zooming  ",Price=0.4m },
+                new Product{Name="Food     ",Price=9.99m },
+                new Product{Name="Qest     ",Price=9.99m},
                 new Product{Name="Soop     ",Price=1.75m ,SupplierID=2001},
                 new Product{Name="Foot     ",Price=1000.01m, SupplierID=2001 },
                 new Product{Name="Botting  ",Price=56.41m,SupplierID=1001 },
-                new Product{Name="Botting 2",Price=null,SupplierID=null }
+                new Product{Name="Botting 2" }
 
             };
         
         }
-        public override string ToString()
-        {
+        //public override string ToString()
+        //{
             
-            return string.Format("{2:D4}    {0}     {1:N2}",
-                Name,Price , SupplierID==null ? 0000:SupplierID);
-        }
+        //    return string.Format("{2:D4}    {0}     {1:N2}",
+        //        Name,Price , SupplierID==null ? 0000:SupplierID);
+        //}
 
 
     }
 
     class Supplier
     {
-        public string Name { get; private set; }
-        public int SupplierID { get; private set; }
-        public Supplier(string name, int supplierID)
+        readonly string name;
+        public string Name => name;
+        
+        readonly int? supplierID;
+        public int? SupplierID => supplierID;
+
+        public Supplier(string name =null, int? supplierID =null)
         {
-            Name = name;
-            SupplierID = supplierID;
+            this.name = name;
+            this.supplierID = supplierID;
         }
-        Supplier() { }
+
+
+
         public static List<Supplier> GetSampleSupplier()
         {
             return new List<Supplier>
             {
-                new Supplier{Name="Apple    ",SupplierID=1001},
-                new Supplier{Name="Microsoft",SupplierID=2001 }
+                new Supplier(name:"Apple    ",supplierID:1001),
+                new Supplier(name:"Microsoft",supplierID:2001)
 
             };
 
         }
-        public override string ToString()
-        {
+        //public override string ToString()
+        //{
 
-            return string.Format("{0}     {1:N2}",Name, SupplierID );
-        }
+        //    return string.Format("{0}     {1:N2}",Name, SupplierID );
+        //}
 
 
     }
